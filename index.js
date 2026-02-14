823064843b24bc112bcb6e6e578f01bbdd3fddac
Jii <201742065+Jii-Jii@users.noreply.github.com>
2026-02-15 02:02:45 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+//First try at javascript.
+
+// let name = "Jii";
+// console.log(name);
+
+// //let-->variable cannot be re-declared but can be updated.
+// let firstName = "Mee";
+// firstName = "Dorae";
+// console.log(firstName);
+
+//const-->variable cannot be re-declared or updated.
+
+
+let person = {
+    name: 'Jii',
+    age: 12
+};
+
+//Dot notation
+person.name = 'Dorae';
+
+//Bracket notaion
+let selection = 'name';
+person[selection] = 'Kiki';
+console.log(person);
+
+
+//array
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 1;
+console.log(selectedColors)
+
+//function
+function greet(name, lastName) {
+    console.log('Hello ' + name + lastName);
+}
+function square(number){
+    return number * number;
+}
+// let number = square(2);
+// console.log(number);
+//OR
+greet('jii ', 'Mee');
+console.log(square(2));
