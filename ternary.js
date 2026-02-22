9018354bbd491f1a33749fe4cde0bdd9c82f77ed
Jii <201742065+Jii-Jii@users.noreply.github.com>
2026-02-23 02:46:26 +0700

    Add: map,filter

--- ternary.js
+++ ternary.js
@@ -55,5 +55,31 @@ const updatedUser = {
   ...user, //shallow copy
   age: 30,
 };
-
 console.log(updatedUser);
+
+//---------map,filter,reduce-----------
+const numbers = [1, 2, 3, 4];
+const doubled = numbers.map((n) => n * 2);
+
+const words = ["apple", "banana"]; // Convert to strings
+const upper = words.map((word) => word.toUpperCase());
+
+const evens = numbers.filter((n) => n % 2 === 0);
+
+const names = ["Joji", "Miko", "Lili"]; // Filter long words
+const longNames = names.filter((name) => name.length > 3);
+
+//with objects
+const users = [
+  { name: "A", score: 10 },
+  { name: "B", score: 20 },
+];
+const updated = users.map((user) => ({
+  ...user,
+  score: user.score + 5,
+}));
+console.log(updated);
+
+console.log("users === updated:", users === updated); //false, because they are different objects in memory.
+
+console.log("users[0] === updated[0]:", users[0] === updated[0]); //false, because they are separate objects in memory, even though they have the same properties and values.
